04ed01d0bbeb1894c95c90cbeb592fef20d6e393
Bridger Maxwell <bridger.maxwell@gmail.com>
2015-02-21 18:14:26 -0500

    Add a scale selector to the number slider.

---
+++ NumberPad/SwiftAutoLayout.swift
@@ -0,0 +1,189 @@
+//  Copyright (c) 2014 Indragie Karunaratne. All rights reserved.
+//  Licensed under the MIT license, see LICENSE file for more info.
+
+#if os(OSX)
+    import AppKit
+    public typealias ALView = NSView
+#elseif os(iOS)
+    import UIKit
+    public typealias ALView = UIView
+#endif
+
+public struct ALLayoutItem {
+    public let view: ALView
+    public let attribute: NSLayoutAttribute
+    public let multiplier: CGFloat
+    public let constant: CGFloat
+    
+    init (view: ALView, attribute: NSLayoutAttribute, multiplier: CGFloat, constant: CGFloat) {
+        self.view = view
+        self.attribute = attribute
+        self.multiplier = multiplier
+        self.constant = constant
+    }
+    
+    init (view: ALView, attribute: NSLayoutAttribute) {
+        self.view = view
+        self.attribute = attribute
+        self.multiplier = 1.0
+        self.constant = 0.0
+    }
+    
+    // relateTo(), equalTo(), greaterThanOrEqualTo(), and lessThanOrEqualTo() used to be overloaded functions
+    // instead of having two separately named functions (e.g. relateTo() and relateToConstant()) but they had
+    // to be renamed due to a compiler bug where the compiler chose the wrong function to call.
+    //
+    // Repro case: http://cl.ly/3S0a1T0Q0S1D
+    // rdar://17412596, OpenRadar: http://www.openradar.me/radar?id=5275533159956480
+    
+    /// Builds a constraint by relating the item to another item.
+    public func relateTo(right: ALLayoutItem, relation: NSLayoutRelation) -> NSLayoutConstraint {
+        return NSLayoutConstraint(item: view, attribute: attribute, relatedBy: relation, toItem: right.view, attribute: right.attribute, multiplier: right.multiplier, constant: right.constant)
+    }
+    
+    /// Builds a constraint by relating the item to a constant value.
+    public func relateToConstant(right: CGFloat, relation: NSLayoutRelation) -> NSLayoutConstraint {
+        return NSLayoutConstraint(item: view, attribute: attribute, relatedBy: relation, toItem: nil, attribute: NSLayoutAttribute.NotAnAttribute, multiplier: 1.0, constant: right)
+    }
+    
+    /// Equivalent to NSLayoutRelation.Equal
+    public func equalTo(right: ALLayoutItem) -> NSLayoutConstraint {
+        return relateTo(right, relation: .Equal)
+    }
+    
+    /// Equivalent to NSLayoutRelation.Equal
+    public func equalToConstant(right: CGFloat) -> NSLayoutConstraint {
+        return relateToConstant(right, relation: .Equal)
+    }
+    
+    /// Equivalent to NSLayoutRelation.GreaterThanOrEqual
+    public func greaterThanOrEqualTo(right: ALLayoutItem) -> NSLayoutConstraint {
+        return relateTo(right, relation: .GreaterThanOrEqual)
+    }
+    
+    /// Equivalent to NSLayoutRelation.GreaterThanOrEqual
+    public func greaterThanOrEqualToConstant(right: CGFloat) -> NSLayoutConstraint {
+        return relateToConstant(right, relation: .GreaterThanOrEqual)
+    }
+    
+    /// Equivalent to NSLayoutRelation.LessThanOrEqual
+    public func lessThanOrEqualTo(right: ALLayoutItem) -> NSLayoutConstraint {
+        return relateTo(right, relation: .LessThanOrEqual)
+    }
+    
+    /// Equivalent to NSLayoutRelation.LessThanOrEqual
+    public func lessThanOrEqualToConstant(right: CGFloat) -> NSLayoutConstraint {
+        return relateToConstant(right, relation: .LessThanOrEqual)
+    }
+}
+
+/// Multiplies the operand's multiplier by the RHS value
+public func * (left: ALLayoutItem, right: CGFloat) -> ALLayoutItem {
+	return ALLayoutItem(view: left.view, attribute: left.attribute, multiplier: left.multiplier * right, constant: left.constant)
+}
+
+/// Divides the operand's multiplier by the RHS value
+public func / (left: ALLayoutItem, right: CGFloat) -> ALLayoutItem {
+	return ALLayoutItem(view: left.view, attribute: left.attribute, multiplier: left.multiplier / right, constant: left.constant)
+}
+
+/// Adds the RHS value to the operand's constant
+public func + (left: ALLayoutItem, right: CGFloat) -> ALLayoutItem {
+	return ALLayoutItem(view: left.view, attribute: left.attribute, multiplier: left.multiplier, constant: left.constant + right)
+}
+
+/// Subtracts the RHS value from the operand's constant
+public func - (left: ALLayoutItem, right: CGFloat) -> ALLayoutItem {
+	return ALLayoutItem(view: left.view, attribute: left.attribute, multiplier: left.multiplier, constant: left.constant - right)
+}
+
+/// Equivalent to NSLayoutRelation.Equal
+public func == (left: ALLayoutItem, right: ALLayoutItem) -> NSLayoutConstraint {
+	return left.equalTo(right)
+}
+
+/// Equivalent to NSLayoutRelation.Equal
+public func == (left: ALLayoutItem, right: CGFloat) -> NSLayoutConstraint {
+    return left.equalToConstant(right)
+}
+
+/// Equivalent to NSLayoutRelation.GreaterThanOrEqual
+public func >= (left: ALLayoutItem, right: ALLayoutItem) -> NSLayoutConstraint {
+	return left.greaterThanOrEqualTo(right)
+}
+
+/// Equivalent to NSLayoutRelation.GreaterThanOrEqual
+public func >= (left: ALLayoutItem, right: CGFloat) -> NSLayoutConstraint {
+    return left.greaterThanOrEqualToConstant(right)
+}
+
+/// Equivalent to NSLayoutRelation.LessThanOrEqual
+public func <= (left: ALLayoutItem, right: ALLayoutItem) -> NSLayoutConstraint {
+	return left.lessThanOrEqualTo(right)
+}
+
+/// Equivalent to NSLayoutRelation.LessThanOrEqual
+public func <= (left: ALLayoutItem, right: CGFloat) -> NSLayoutConstraint {
+    return left.lessThanOrEqualToConstant(right)
+}
+
+public extension ALView {
+    func al_operand(attribute: NSLayoutAttribute) -> ALLayoutItem {
+        return ALLayoutItem(view: self, attribute: attribute)
+    }
+    
+    /// Equivalent to NSLayoutAttribute.Left
+    var al_left: ALLayoutItem {
+        return al_operand(.Left)
+    }
+    
+    /// Equivalent to NSLayoutAttribute.Right
+    var al_right: ALLayoutItem {
+        return al_operand(.Right)
+    }
+    
+    /// Equivalent to NSLayoutAttribute.Top
+    var al_top: ALLayoutItem {
+        return al_operand(.Top)
+    }
+    
+    /// Equivalent to NSLayoutAttribute.Bottom
+    var al_bottom: ALLayoutItem {
+        return al_operand(.Bottom)
+    }
+    
+    /// Equivalent to NSLayoutAttribute.Leading
+    var al_leading: ALLayoutItem {
+        return al_operand(.Leading)
+    }
+    
+    /// Equivalent to NSLayoutAttribute.Trailing
+    var al_trailing: ALLayoutItem {
+        return al_operand(.Trailing)
+    }
+    
+    /// Equivalent to NSLayoutAttribute.Width
+    var al_width: ALLayoutItem {
+        return al_operand(.Width)
+    }
+    
+    /// Equivalent to NSLayoutAttribute.Height
+    var al_height: ALLayoutItem {
+        return al_operand(.Height)
+    }
+    
+    /// Equivalent to NSLayoutAttribute.CenterX
+    var al_centerX: ALLayoutItem {
+        return al_operand(.CenterX)
+    }
+    
+    /// Equivalent to NSLayoutAttribute.CenterY
+    var al_centerY: ALLayoutItem {
+        return al_operand(.CenterY)
+    }
+    
+    /// Equivalent to NSLayoutAttribute.Baseline
+    var al_baseline: ALLayoutItem {
+        return al_operand(.Baseline)
+    }
+}
---
+++ NumberPad/VisualFormat.swift
@@ -0,0 +1,436 @@
+//
+//  VisualFormat.swift
+//  SwiftVisualFormat
+//
+//  Created by Bridger Maxwell on 8/1/14.
+//  Copyright (c) 2014 Bridger Maxwell. All rights reserved.
+//
+
+#if os(OSX)
+    import AppKit
+    public typealias ALVFView = NSView
+    #elseif os(iOS)
+    import UIKit
+    public typealias ALVFView = UIView
+#endif
+
+// layoutHorizontal(|[imageView.al >= 20.al]-(>=0.al!20.al)-[imageView.al]-50.al-|)
+
+extension ALVFView {
+    public func addVerticalConstraints(constraintAble: [ConstraintAble]) {
+        self.addConstraints(verticalConstraints(constraintAble))
+    }
+    
+    public func addHorizontalConstraints(constraintAble: [ConstraintAble]) {
+        self.addConstraints(horizontalConstraints(constraintAble))
+    }
+}
+
+@objc public protocol ConstraintAble {
+    func toConstraints(axis: UILayoutConstraintAxis) -> [NSLayoutConstraint];
+}
+
+public func constraints(axis: UILayoutConstraintAxis, constraintAble: [ConstraintAble]) -> [NSLayoutConstraint] {
+    return constraintAble[0].toConstraints(axis)
+}
+
+public func horizontalConstraints(constraintAble: [ConstraintAble]) -> [NSLayoutConstraint] {
+    return constraints(.Horizontal, constraintAble)
+}
+
+public func verticalConstraints(constraintAble: [ConstraintAble]) -> [NSLayoutConstraint] {
+    return constraints(.Vertical, constraintAble)
+}
+
+
+@objc public protocol ViewContainingToken {
+    var firstView: ALVFView? { get }
+    var lastView: ALVFView? { get }
+}
+
+protocol ConstantToken {
+    var ALConstant: CGFloat { get }
+}
+
+// This is half of a space constraint, [view]-space
+class ViewAndSpaceToken {
+    let view: ViewContainingToken
+    let space: ConstantToken
+    let relation: NSLayoutRelation
+    init(view: ViewContainingToken, space: ConstantToken, relation: NSLayoutRelation) {
+        self.view = view
+        self.space = space
+        self.relation = relation
+    }
+}
+
+// This is half of a space constraint, |-5
+class LeadingSuperviewAndSpaceToken {
+    let space: ConstantToken
+    let relation: NSLayoutRelation
+    init(space: ConstantToken, relation: NSLayoutRelation) {
+        self.space = space
+        self.relation = relation
+    }
+}
+// This is half of a space constraint, 5-|
+class TrailingSuperviewAndSpaceToken {
+    let space: ConstantToken
+    init(space: ConstantToken) {
+        self.space = space
+    }
+}
+
+// [view]-5-[view2]
+@objc class SpacedViewsConstraintToken: ConstraintAble, ViewContainingToken {
+    let leadingView: ViewContainingToken
+    let trailingView: ViewContainingToken
+    let space: ConstantToken
+    
+    init(leadingView: ViewContainingToken, trailingView: ViewContainingToken, space: ConstantToken) {
+        self.leadingView = leadingView
+        self.trailingView = trailingView
+        self.space = space
+    }
+    
+    var firstView: UIView? {
+        get {
+            return self.leadingView.firstView
+        }
+    }
+    var lastView: UIView? {
+        get {
+            return self.trailingView.lastView
+        }
+    }
+    
+    
+    @objc func toConstraints(axis: UILayoutConstraintAxis) -> [NSLayoutConstraint] {
+        if let leadingView = self.leadingView.lastView {
+            if let trailingView = self.trailingView.firstView {
+                let space = self.space.ALConstant
+                
+                var leadingAttribute: NSLayoutAttribute!
+                var trailingAttribute: NSLayoutAttribute!
+                if (axis == .Horizontal) {
+                    leadingAttribute = .Leading
+                    trailingAttribute = .Trailing
+                } else {
+                    leadingAttribute = .Top
+                    trailingAttribute = .Bottom
+                }
+                
+                var constraints = [NSLayoutConstraint(
+                    item: trailingView, attribute: leadingAttribute,
+                    relatedBy: .Equal,
+                    toItem: leadingView, attribute: trailingAttribute,
+                    multiplier: 1.0, constant: space)]
+                
+                if let leadingConstraint = self.leadingView as? ConstraintAble {
+                    constraints += leadingConstraint.toConstraints(axis)
+                }
+                if let trailingConstraint = self.trailingView as? ConstraintAble {
+                    constraints += trailingConstraint.toConstraints(axis)
+                }
+                
+                return constraints
+            }
+        }
+        
+        NSException(name: NSInvalidArgumentException, reason: "This space constraint was between two view items that couldn't fit together. Weird?", userInfo: nil).raise()
+        return [] // To appease the compiler, which doesn't realize this branch dies
+    }
+}
+
+// [view == 50]
+@objc class SizeConstantConstraintToken: ConstraintAble, ViewContainingToken {
+    let view: ALVFView
+    let size: ConstantToken
+    let relation: NSLayoutRelation
+    init(view: ALVFView, size: ConstantToken, relation: NSLayoutRelation) {
+        self.view = view
+        self.size = size
+        self.relation = relation
+    }
+    
+    var firstView: ALVFView? {
+        get {
+            return self.view.firstView
+        }
+    }
+    var lastView: ALVFView? {
+        get {
+            return self.view.lastView
+        }
+    }
+    
+    func toConstraints(axis: UILayoutConstraintAxis) -> [NSLayoutConstraint] {
+        let constant = self.size.ALConstant
+        
+        var attribute: NSLayoutAttribute!
+        if (axis == .Horizontal) {
+            attribute = .Width
+        } else {
+            attribute = .Height
+        }
+        let constraint = NSLayoutConstraint(
+            item: self.view, attribute: attribute,
+            relatedBy: self.relation,
+            toItem: nil, attribute: .NotAnAttribute,
+            multiplier: 1.0, constant: constant)
+        
+        return [constraint]
+    }
+    
+}
+
+// [view == view2]
+@objc class SizeRelationConstraintToken: ConstraintAble, ViewContainingToken {
+    let view: ALVFView
+    let relatedView: ALVFView
+    let relation: NSLayoutRelation
+    init(view: ALVFView, relatedView: ALVFView, relation: NSLayoutRelation) {
+        self.view = view
+        self.relatedView = relatedView
+        self.relation = relation
+    }
+    
+    var firstView: ALVFView? {
+        get {
+            return self.view.firstView
+        }
+    }
+    var lastView: ALVFView? {
+        get {
+            return self.view.lastView
+        }
+    }
+    
+    func toConstraints(axis: UILayoutConstraintAxis) -> [NSLayoutConstraint] {
+        var attribute: NSLayoutAttribute!
+        if (axis == .Horizontal) {
+            attribute = .Width
+        } else {
+            attribute = .Height
+        }
+        return [ NSLayoutConstraint(
+            item: self.view, attribute: attribute,
+            relatedBy: self.relation,
+            toItem: self.relatedView, attribute: attribute,
+            multiplier: 1.0, constant: 0) ]
+    }
+}
+
+// |-5-[view]
+@objc public class LeadingSuperviewConstraintToken: ConstraintAble, ViewContainingToken {
+    let viewContainer: ViewContainingToken
+    let space: ConstantToken
+    init(viewContainer: ViewContainingToken, space: ConstantToken) {
+        self.viewContainer = viewContainer
+        self.space = space
+    }
+    public var firstView: UIView? {
+        get {
+            return nil // No one can bind to our first view, is the superview
+        }
+    }
+    public var lastView: UIView? {
+        get {
+            return self.viewContainer.lastView
+        }
+    }
+    
+    public func toConstraints(axis: UILayoutConstraintAxis) -> [NSLayoutConstraint] {
+        if let view = self.viewContainer.firstView {
+            let constant = self.space.ALConstant
+            
+            if let superview = view.superview {
+                var constraint: NSLayoutConstraint!
+                
+                if (axis == .Horizontal) {
+                    constraint = NSLayoutConstraint(
+                        item: view, attribute: .Leading,
+                        relatedBy: .Equal,
+                        toItem: superview, attribute: .Leading,
+                        multiplier: 1.0, constant: constant)
+                } else {
+                    constraint = NSLayoutConstraint(
+                        item: view, attribute: .Top,
+                        relatedBy: .Equal,
+                        toItem: superview, attribute: .Top,
+                        multiplier: 1.0, constant: constant)
+                }
+                
+                if let otherConstraint = viewContainer as?  ConstraintAble {
+                    return otherConstraint.toConstraints(axis) + [constraint]
+                } else {
+                    return [constraint]
+                }
+            }
+            NSException(name: NSInvalidArgumentException, reason: "You tried to create a constraint to \(view)'s superview, but it has no superview yet!", userInfo: nil).raise()
+        }
+        NSException(name: NSInvalidArgumentException, reason: "This superview bar | was before something that doesn't have a view. Weird?", userInfo: nil).raise()
+        return [] // To appease the compiler, which doesn't realize this branch dies
+    }
+    
+    
+}
+
+// [view]-5-|
+@objc public class TrailingSuperviewConstraintToken: ConstraintAble, ViewContainingToken {
+    let viewContainer: ViewContainingToken
+    let space: ConstantToken
+    init(viewContainer: ViewContainingToken, space: ConstantToken) {
+        self.viewContainer = viewContainer
+        self.space = space
+    }
+    public var firstView: UIView? {
+        get {
+            return self.viewContainer.firstView
+        }
+    }
+    public var lastView: UIView? {
+        get {
+            return nil // No one can bind to our last view, is the superview
+        }
+    }
+    
+    public func toConstraints(axis: UILayoutConstraintAxis) -> [NSLayoutConstraint] {
+        if let view = self.viewContainer.lastView {
+            let constant = self.space.ALConstant
+            
+            if let superview = view.superview {
+                var constraint: NSLayoutConstraint!
+                
+                if (axis == .Horizontal) {
+                    constraint = NSLayoutConstraint(
+                        item: superview, attribute: .Trailing,
+                        relatedBy: .Equal,
+                        toItem: view, attribute: .Trailing,
+                        multiplier: 1.0, constant: constant)
+                } else {
+                    constraint = NSLayoutConstraint(
+                        item: superview, attribute: .Bottom,
+                        relatedBy: .Equal,
+                        toItem: view, attribute: .Bottom,
+                        multiplier: 1.0, constant: constant)
+                }
+                
+                if let otherConstraint = viewContainer as?  ConstraintAble {
+                    return otherConstraint.toConstraints(axis) + [constraint]
+                } else {
+                    return [constraint]
+                }
+            }
+            NSException(name: NSInvalidArgumentException, reason: "You tried to create a constraint to \(view)'s superview, but it has no superview yet!", userInfo: nil).raise()
+        }
+        NSException(name: NSInvalidArgumentException, reason: "This superview bar | was after something that doesn't have a view. Weird?", userInfo: nil).raise()
+        
+        return [] // To appease the compiler, which doesn't realize this branch dies
+    }
+}
+
+let RequiredPriority: Float = 1000 // For some reason, the linker can't find UILayoutPriorityRequired. Not sure what I am doing wrong
+
+prefix operator | {}
+prefix public func | (tokenArray: [ViewContainingToken]) -> [LeadingSuperviewConstraintToken] {
+    // |[view]
+    return [LeadingSuperviewConstraintToken(viewContainer: tokenArray[0], space: 0)]
+}
+
+postfix operator | {}
+postfix public func | (tokenArray: [ViewContainingToken]) -> [TrailingSuperviewConstraintToken] {
+    // [view]|
+    return [TrailingSuperviewConstraintToken(viewContainer: tokenArray[0], space: 0)]
+}
+
+func >= (left: ALVFView, right: ConstantToken) -> SizeConstantConstraintToken {
+    // [view >= 50]
+    return SizeConstantConstraintToken(view: left, size: right, relation: .GreaterThanOrEqual)
+}
+func >= (left: ALVFView, right: ALVFView) -> SizeRelationConstraintToken {
+    // [view >= view2]
+    return SizeRelationConstraintToken(view: left, relatedView: right, relation: .GreaterThanOrEqual)
+}
+
+func <= (left: ALVFView, right: ConstantToken) -> SizeConstantConstraintToken {
+    // [view <= 50]
+    return SizeConstantConstraintToken(view: left, size: right, relation: .LessThanOrEqual)
+}
+func <= (left: ALVFView, right: ALVFView) -> SizeRelationConstraintToken {
+    // [view <= view2]
+    return SizeRelationConstraintToken(view: left, relatedView: right, relation: .LessThanOrEqual)
+}
+
+func == (left: ALVFView, right: ConstantToken) -> SizeConstantConstraintToken {
+    // [view == 50]
+    return SizeConstantConstraintToken(view: left, size: right, relation: .Equal)
+}
+func == (left: ALVFView, right: ALVFView) -> SizeRelationConstraintToken {
+    // [view == view2]
+    return SizeRelationConstraintToken(view: left, relatedView: right, relation: .Equal)
+}
+
+func - (left: [ViewContainingToken], right: ConstantToken) -> ViewAndSpaceToken {
+    // [view]-5
+    return ViewAndSpaceToken(view: left[0], space: right, relation: .Equal)
+}
+
+func - (left: ViewAndSpaceToken, right: [ViewContainingToken]) -> [SpacedViewsConstraintToken] {
+    // [view]-5-[view2]
+    return [SpacedViewsConstraintToken(leadingView: left.view, trailingView: right[0], space: left.space)]
+}
+
+func - (left: [ViewContainingToken], right: TrailingSuperviewAndSpaceToken) -> [TrailingSuperviewConstraintToken] {
+    // [view]-5-|
+    return [TrailingSuperviewConstraintToken(viewContainer: left[0], space: right.space)]
+}
+
+func - (left: LeadingSuperviewAndSpaceToken, right: [ViewContainingToken]) -> [LeadingSuperviewConstraintToken] {
+    // |-5-[view]
+    return [LeadingSuperviewConstraintToken(viewContainer: right[0], space: left.space)]
+}
+
+postfix operator -| {}
+postfix func -| (constant: ConstantToken) -> TrailingSuperviewAndSpaceToken {
+    // 5-|
+    return TrailingSuperviewAndSpaceToken(space: constant)
+}
+
+prefix operator |- {}
+prefix func |- (constant: ConstantToken) -> LeadingSuperviewAndSpaceToken {
+    // |-5
+    return LeadingSuperviewAndSpaceToken(space: constant, relation: .Equal)
+}
+
+
+extension ALVFView: ViewContainingToken {
+    
+    public var firstView: ALVFView? {
+        get {
+            return self
+        }
+    }
+    public var lastView: ALVFView? {
+        get {
+            return self
+        }
+    }
+}
+
+extension CGFloat: ConstantToken {
+    var ALConstant: CGFloat {
+        get {
+            return self
+        }
+    }
+}
+
+extension NSInteger: ConstantToken {
+    var ALConstant: CGFloat {
+        get {
+            return CGFloat(self)
+        }
+    }
+}
+
