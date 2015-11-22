59c13214e115824578bbd19f323f3a41c191af3a
Bridger Maxwell <bridger.maxwell@gmail.com>
2015-11-22 00:10:31 -0500

    Add an intro screen before the canvas.

--- NumberPad/AppDelegate.swift
+++ NumberPad/AppDelegate.swift
@@ -7,23 +7,12 @@
 //
 
 import UIKit
-import DigitRecognizerSDK
-
-let filePrefix = "SavedLibraries-"
 
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-    var rootViewController: ViewController!
-
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        self.rootViewController = window?.rootViewController as! ViewController
-        
-        if let path = NSBundle.mainBundle().pathForResource("bridger_normalized", ofType: "json") {
-            loadData(path)
-        }
-        
         // Set up a few more rewrite rules
         let expressionRewriter = DDExpressionRewriter.defaultRewriter()
         
@@ -46,39 +35,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         expressionRewriter.addRewriteRule("__exp1 * (__exp2 - __exp3)", forExpressionsMatchingTemplate:"__exp2 * __exp1 - __exp1 * __exp3", condition:nil)
         
         return true
-    }
-    
-    func documentsDirectory() -> String {
-        return NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.UserDomainMask, true)[0] as String
-    }
-    
-    func loadData(path: String) {
-        if let jsonLibrary = DTWDigitClassifier.jsonLibraryFromFile(path) {
-            self.rootViewController.digitClassifier.loadData(jsonLibrary, loadNormalizedData: true)
-        }
-    }
-    
-    
-    func newestSavedData() -> String? {
-        let contents: [AnyObject]?
-        do {
-            contents = try NSFileManager.defaultManager().contentsOfDirectoryAtPath(self.documentsDirectory())
-        } catch _ {
-            contents = nil
-        }
-        if contents != nil {
-            if let contents = contents as? [String] {
-                let contents = contents.filter({ string in
-                    return string.hasPrefix(filePrefix)
-                }).sort({ (string1, string2) in
-                    return string1.compare(string2) == NSComparisonResult.OrderedAscending
-                })
-                
-                return contents.last
-            }
-        }
-        
-        return nil
     }
 }
 
