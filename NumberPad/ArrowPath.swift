31f3f9ffc58fb808220f56d734cc6f5d95572a44
Bridger Maxwell <bridger.maxwell@gmail.com>
2016-06-01 09:45:59 -0400

    Fix dashed line.

--- NumberPad/ArrowPath.swift
+++ NumberPad/ArrowPath.swift
@@ -20,13 +20,12 @@ func createPointingLine(startPoint: CGPoint, endPoint: CGPoint, dash: Bool, arro
     path.moveTo(nil, x: 0, y: 0)
     path.addLineTo(nil, x: length, y: 0)
     
-    // TODO: Fix in swift3
-//    if dash {
-//        let dashPattern: [CGFloat] = [4, 6]
-//        if let dashedPath = CGPath(copyByDashingPath: path, transform: nil, phase: 0, lengths: dashPattern, count: dashPattern.count) {
-//            path = dashedPath.mutableCopy()
-//        }
-//    }
+    if dash {
+        let dashPattern: [CGFloat] = [4, 6]
+        if let dashedPath = CGPath(copyByDashingPath: path, transform: nil, phase: 0, lengths: dashPattern, count: dashPattern.count)?.mutableCopy() {
+            path = dashedPath
+        }
+    }
     
     if arrowHead {
         /* Now add the arrow head
